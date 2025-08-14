0239b138de525880de4584933bb2df0671f31e36
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-08-14 22:48:55 +0100

    prefall component

--- src/Components/prefall.jsx
+++ src/Components/prefall.jsx
@@ -2,19 +2,36 @@
 
 export default function Prefall(){
     return (
-        <div className="prefall m-2 ">
-            <div className="prefall-header d-flex  ">
-                <div className="first-section">
-
+        <div className="prefall  ">
+            {/* Prefall Header Jsx */}
+            <div className="prefall-header m-2 d-flex  ">
+                <div className="first-section d-flex justify-content-center align-items-center">
+                    <p className="fs-4">Fall</p>
                 </div>
  
-                <div className="second-section bg-danger">
-                    
+                <div className="second-section d-flex justify-content-between align-items-center p-2">
+                    <p className="fs-4 text-black text-uppercase">
+                        pre fall 2024 
+                    </p>
+
+                    <hr className=" prefall-line w-50 " />
+
+                    <button className="prefall-header-button p-2">
+                        Explore
+                    </button>
                 </div>
 
-                <div className="Third-section bg-info">
-                    
+                <div className="Third-section d-flex justify-content-center align-items-center">
+                    <p className="fs-4">2024</p>
                 </div>
+            </div>
+
+            {/* Prefall Header Jsx */}
+            <div className="prefall-dress-wrapper row m-2 flex">
+                <div className="prefall-dress col-12">
+                
+                </div>
+
             </div>
         </div>
     )
