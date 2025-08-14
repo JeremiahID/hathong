392395a24770f4f208f15b3869bbc66d913bf7b1
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-08-14 23:10:43 +0100

    prefall component

--- src/Components/prefall.jsx
+++ src/Components/prefall.jsx
@@ -28,10 +28,47 @@ export default function Prefall(){
 
             {/* Prefall Header Jsx */}
             <div className="prefall-dress-wrapper row m-2 flex">
-                <div className="prefall-dress col-12">
-                
-                </div>
+                <div className="prefall-dress col-12 ">
+                    <div className="prefall-dress row">
+                        {/* First Dress */}
+                        <div className="prefall-first-dress bg-secondary col-3 ">
 
+                        </div>
+
+                        {/* Second Dress */}
+                        <div className="prefall-second-dress bg-dark col-3 ">
+
+                        </div>
+                        
+                        {/* Third Dress */}
+                        <div className="prefall-third-dress bg-success col-3 ">
+
+                        </div>
+
+                        {/* Text Content */}
+                        <div className="prefall-text-content  col-3 ">
+                            <div class="text-content row ">
+                                <div class="text-content-one col-12 border flex  flex-column align-items-baseline ">
+                                    <p class=" px-4 fs-8">"she may be the song that summer sings</p>
+                                    <p class=" px-4 fs-8">May be the chill that autumn brings</p>
+                                    <p class=" px-4 fs-8">May be a hundred different things...."</p>
+                
+                                </div>
+                                <div class="text-content-two col-12 border flex flex-column">
+                                    <div class="text-content-about">
+                                        <p class="fs-8 text-center">
+                                            "Ha Thong" officially launched in 
+                                            December 2019 is a fashion brand
+                                            founded by the designer of the same 
+                                            name.
+                                        </p>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
