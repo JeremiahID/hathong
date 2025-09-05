9b723850ae973580e4a46b5e5ac890cde604e21e
Idahosa Jeremiah Efe <jeremiahefe.je@gmail.com>
2025-09-05 21:57:35 +0100

    vision statemet

--- src/Components/prefall.jsx
+++ src/Components/prefall.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import prefallFirstCloth from '../Images/Main-page/prefall-first-dress.jpg';
 import prefallFirstClothModel from '../Images/Main-page/prefall-first-dress-model.jpg';
 import prefallSecondCloth from '../Images/Main-page/prefall-second-dress.jpg';
@@ -99,7 +99,11 @@ export default function Prefall(){
         };
     }, []);
 
+    const [fillFavorite, setfillFavorite] = useState()
 
+    function toggleFavorite(){
+
+    }
 
     return (
         <div className="prefall  ">
@@ -132,7 +136,11 @@ export default function Prefall(){
                     <div className="prefall-dress row">
                         {/* First Dress */}
                         <div  ref={prefallFirstDress}  className="prefall-first-dress col-3 border border-1 ">
-
+                            <button className='button-fav' >
+                                <svg className='favorite-icon' id="favorite-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                </svg>
+                            </button>
                         </div>
 
                         {/* Second Dress */}
